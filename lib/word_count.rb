497393dbb7a4c5f973e0887ec5792fa89de5619a
Margie Nevarez <margie.ynm@gmail.com>
2016-11-18 10:20:23 -0800

    return correct count of word while taking into account partial matches

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,10 +1,12 @@
 class String
   define_method(:word_count) do |target|
     count = 0
-    phrase = self
+    phrase = self.downcase()
     phrase = phrase.split(' ').each do |word|
-      if word == target
-        count += 1
+      if word == target.downcase()
+        count +=1
+      elsif word.include?(target)
+        count +=1
       end
     end
     count
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -11,4 +11,19 @@ describe('String#word_count') do
   it('will compare a string of multiple words with a single word string and return a count of 1 for its number of occurrences in first string') do
     expect("keep calm and quack on".word_count("quack")).to(eq(1))
   end
+  it('will compare a string of multiple words with a single word string and return a count of 0 for its number of occurrences in first string') do
+    expect("keep calm and quack on".word_count("yo")).to(eq(0))
+  end
+  it('will compare a string of multiple words with a single word string and return a count of 3 for its number of occurrences in first string') do
+    expect("keep calm and quack quack quack on".word_count("quack")).to(eq(3))
+  end
+  it('will compare a string of multiple words with a single word string and return a count of 2 despite capitalization') do
+    expect("KEEP CALM and QUACK QUACK ON".word_count("quack")).to(eq(2))
+  end
+  it('will compare a string of multiple words with a single word string and return a count of 4 despite capitalization') do
+    expect("keep calm and quack quack quack quack on".word_count("QUACK")).to(eq(4))
+  end
+  it('will compare a string of multiple words with a single word string and return a count of 2 taking into account partial matches') do
+    expect("Hello Ducks, welcome to introDUCKtion!".word_count("duck")).to(eq(2))
+  end
 end
