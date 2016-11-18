4c7d091e21e5b94ddd93a756770de89596d84d17
Margie Nevarez <margie.ynm@gmail.com>
2016-11-18 09:29:40 -0800

    return a count of 1 if single word string is included in the string of multiple words

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,9 +1,11 @@
 class String
   define_method(:word_count) do |target|
     count = 0
-    word = self
-    if word == target
-      count += 1
+    phrase = self
+    phrase = phrase.split(' ').each do |word|
+      if word == target
+        count += 1
+      end
     end
     count
   end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -8,7 +8,7 @@ describe('String#word_count') do
   it('will compare first string with a different second string and return a count of zero for its number of occurences') do
     expect("duck".word_count("dog")).to(eq(0))
   end
-  it('will compare first string with a string of multiple words and return a count of 1 for its number of occurrences in second string') do
+  it('will compare a string of multiple words with a single word string and return a count of 1 for its number of occurrences in first string') do
     expect("keep calm and quack on".word_count("quack")).to(eq(1))
   end
 end
