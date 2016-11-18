6e0cc850afd474b4eff9ff0fcd64077439ec5f4e
Margie Nevarez <margie.ynm@gmail.com>
2016-11-18 08:53:02 -0800

    return a count of 1 if first string and second string match

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,5 +1,10 @@
 class String
-  define_method(:word_count) do
-    
+  define_method(:word_count) do |target|
+    count = 0
+    word = self
+    if word == target
+      count += 1
+    end
+    count
   end
 end
