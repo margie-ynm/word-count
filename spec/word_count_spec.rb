0d92709f4064ef5d8338515013a523fd484fe52f
Margie Nevarez <margie.ynm@gmail.com>
2016-11-18 08:45:50 -0800

    include second spec to pass after line of code

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,6 +3,9 @@ require('word_count')
 
 describe('String#word_count') do
   it('will compare first string with an identical second string and return a count of one for its number of occurences') do
-    expect("duck".word_count('duck')).to(eq(1))
+    expect("duck".word_count("duck")).to(eq(1))
+  end
+  it('will compare first string with a different second string and return a count of zero for its number of occurences') do
+    expect("duck".word_count("dog")).to(eq(0))
   end
 end
